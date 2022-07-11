c405a463c21875a314719557d03a829da8b63a56
guillo93 <gmllugdar@gmail.com>
2022-07-11 12:11:26 -0300

    ....

--- api/src/db.js
+++ api/src/db.js
@@ -14,7 +14,6 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: 5432,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
